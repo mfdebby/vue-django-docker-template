2b504b847fc4e7cbbdcefef34ab15b7ab5201181
mfwaltzfordebby <newstartagainhays@gmail.com>
2025-01-15 04:44:16 +0800

    chore: setup mypy

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ lint:
 	@uv run black --line-length 80 backend/.
 	@uv run isort backend/.
 	@uv run ruff check backend/.
+	@uv run mypy --explicit-package-bases \
+		--fast-module-lookup \
+		--ignore-missing-imports \
+		--strict backend/.
--- backend/template_app/management/commands/create_superuser_if_not_exists.py
+++ backend/template_app/management/commands/create_superuser_if_not_exists.py
@@ -4,10 +4,10 @@ from django.core.management.base import BaseCommand
 from loguru import logger
 
 
-class Command(BaseCommand):
+class Command(BaseCommand):  # type: ignore[misc]
     help = "Create a superuser if one does not exist"
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
         username = config("DJANGO_SUPERUSER_USERNAME")
         email = config("DJANGO_SUPERUSER_EMAIL")
         password = config("DJANGO_SUPERUSER_PASSWORD")
