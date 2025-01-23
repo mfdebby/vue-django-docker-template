9967bcfba7231daf85f74d2cf4865658799eb3f9
mfdebby <newstartagainhays@gmail.com>
2025-01-23 12:18:53 +0800

    chore: add static url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for core project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
